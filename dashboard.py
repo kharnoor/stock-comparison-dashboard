e7d2843d274a00cb050f0c31d7d5b80796ce664b
Kaustubh Harnoor <harnoorkaustubh@gmail.com>
2022-08-24 16:04:44 -0700

    Change the axis to relative returns

--- dashboard.py
+++ dashboard.py
@@ -10,8 +10,19 @@ dropdown = st.multiselect('Pick your stocks', tickers)
 
 #change to last years's date
 start = st.date_input('Start', value = pd.to_datetime('01/01/2022'))
-end = st.date_input('Start', value = pd.to_datetime('today'))
+end = st.date_input('End', value = pd.to_datetime('today'))
 
-if len(dropdown) > 0:
+def relativeReturns(df):
+    rel = df.pct_change()
+    cumulRet = (1+rel).cumprod() - 1
+    cumulRet = cumulRet.fillna(0)
+    return cumulRet
+
+if len(dropdown) == 1:
     df = yf.download(dropdown, start, end) ['Adj Close']
+    st.line_chart(df)
+
+if len(dropdown) > 1:
+    #df = yf.download(dropdown, start, end) ['Adj Close']
+    df = relativeReturns(yf.download(dropdown, start, end) ['Adj Close'])
     st.line_chart(df)
