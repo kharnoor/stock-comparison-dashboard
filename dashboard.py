7e87123e226c3a3696b575a528a39675e3dd18c7
Kaustubh Harnoor <harnoorkaustubh@gmail.com>
2022-08-24 15:52:00 -0700

    Graph the stock price given a stock, start date, and end date

--- dashboard.py
+++ dashboard.py
@@ -1,6 +1,17 @@
-import streamlit as sl 
-import yfinance as yfinanc
+import streamlit as st 
+import yfinance as yf
 import pandas as pd
+#import arrow as a
+st.title("Finance Dashboard")
 
-sl.title("Finance Dashboard")
+tickers = ('TSLA', 'AAPL', 'MSFT', 'GOOG', 'AMZN', 'META', 'V', 'NVDA')
 
+dropdown = st.multiselect('Pick your stocks', tickers)
+
+#change to last years's date
+start = st.date_input('Start', value = pd.to_datetime('01/01/2022'))
+end = st.date_input('Start', value = pd.to_datetime('today'))
+
+if len(dropdown) > 0:
+    df = yf.download(dropdown, start, end) ['Adj Close']
+    st.line_chart(df)
