fc998d1b85e4659097ac885d875e6fd4842511c6
Kaustubh Harnoor <harnoorkaustubh@gmail.com>
2022-08-24 16:18:22 -0700

    View both Culumalative and Relative returns

--- dashboard.py
+++ dashboard.py
@@ -9,8 +9,8 @@ tickers = ('TSLA', 'AAPL', 'MSFT', 'GOOG', 'AMZN', 'META', 'V', 'NVDA')
 dropdown = st.multiselect('Pick your stocks', tickers)
 
 #change to last years's date
-start = st.date_input('Start', value = pd.to_datetime('01/01/2022'))
-end = st.date_input('End', value = pd.to_datetime('today'))
+start = st.date_input('Start Data', value = pd.to_datetime('01/01/2022'))
+end = st.date_input('End Date', value = pd.to_datetime('today'))
 
 def relativeReturns(df):
     rel = df.pct_change()
@@ -18,11 +18,19 @@ def relativeReturns(df):
     cumulRet = cumulRet.fillna(0)
     return cumulRet
 
-if len(dropdown) == 1:
-    df = yf.download(dropdown, start, end) ['Adj Close']
-    st.line_chart(df)
+#if len(dropdown) == 1:
+ #   df1 = yf.download(dropdown, start, end) ['Adj Close']
+    #st.header("Cumulative Returns of {}".format(dropdown))
+  #  st.line_chart(df1)
 
-if len(dropdown) > 1:
+if len(dropdown) > 0:
     #df = yf.download(dropdown, start, end) ['Adj Close']
+    
+    for i in dropdown:
+        df1 = yf.download(i, start, end) ['Adj Close']
+        st.header("Cumulative Returns of {}".format(i))
+        st.line_chart(df1)
+        
     df = relativeReturns(yf.download(dropdown, start, end) ['Adj Close'])
+    st.header("Relative Returns of {}".format(dropdown))
     st.line_chart(df)
