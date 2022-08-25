2cdf31c28db8df737712fb239b849ea442886147
Kaustubh Harnoor <harnoorkaustubh@gmail.com>
2022-08-24 21:41:53 -0700

    Fix typos, add text about information about the graphs

--- dashboard.py
+++ dashboard.py
@@ -1,15 +1,15 @@
 import streamlit as st 
 import yfinance as yf
 import pandas as pd
-#import arrow as a
+
 st.title("Finance Dashboard")
 
 tickers = ('TSLA', 'AAPL', 'MSFT', 'GOOG', 'AMZN', 'META', 'V', 'NVDA')
-
+st.write("Cumulative returns are the total change in the stock price between the selected start and end date. Although this visualizes the change in stock price, it is not useful for comparing the performance of multiple stocks")
 dropdown = st.multiselect('Pick your stocks', tickers)
 
 #change to last years's date
-start = st.date_input('Start Data', value = pd.to_datetime('01/01/2022'))
+start = st.date_input('Start Date', value = pd.to_datetime('01/01/2022'))
 end = st.date_input('End Date', value = pd.to_datetime('today'))
 
 def relativeReturns(df):
@@ -33,4 +33,7 @@ if len(dropdown) > 0:
         
     df = relativeReturns(yf.download(dropdown, start, end) ['Adj Close'])
     st.header("Relative Returns of {}".format(dropdown))
+    st.write("Relative returns present the returns on an investment compared to a benchmark. Because the data are on the same axis, the Relative Returns graph makes comparing perfomances of multiple stocks easier.")
     st.line_chart(df)
+
+#notes for tomorrow: check st and yf documentation for info about stocks details (+/- whatever), add header/subtitle w more info
